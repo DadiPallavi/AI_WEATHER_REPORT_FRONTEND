f2e47eda4302ada9435b628db6e6428753b0b6be
DadiPallavi <dadipallavi5@gmail.com>
2026-06-06 11:52:23 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import requests as rq
+server_url=""
+st.title("AI_WEATHER_FORECASTING")
+city=st.text_input("enter city")
+question=st.text_input("ask your question..")
+if st.button("ask ai"):
+    res=rq.post(f"{server_url}/get_weather",params={
+        "city":city,
+        "question":question
+    })
+    st.success(res.json()["messages"][-1]["content"])
+    
